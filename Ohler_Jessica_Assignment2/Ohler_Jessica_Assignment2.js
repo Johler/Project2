744d339db77392308e82b5e60e32c88eecdb8374
Jessica Ohler <jessicaohler@jessicahlersmbp.gateway.pace.com>
2014-07-17 09:35:15 -0400

    Added Boolean Function

--- Ohler_Jessica_Assignment2/Ohler_Jessica_Assignment2.js
+++ Ohler_Jessica_Assignment2/Ohler_Jessica_Assignment2.js
@@ -9,9 +9,10 @@
 //Set variables
 var partnerName = "Jabba the Pug",
 	expeditionPartner = true,
-	friendCompanion = true
+	friendCompanion = true,
+	aliensHealth = 100
 ;
-//Prompt finding out if user would like to travel with a companion.
+//Prompt: Finding out if user would like to travel with a companion.
 myConfirmation = confirm("Would you like to time travel with an expedition partner today?");
 if (expeditionPartner === true) {
 	console.log("Let's decide who to pick!")
@@ -19,7 +20,7 @@ if (expeditionPartner === true) {
 	console.log("Alright, let's get traveling alone!")
 }
 
-//Procedure which companion to travel with.
+//Procedure: which companion to travel with.
 if (friendCompanion === true) {
 	var friendCompanion = true,
 		loverCompanion = false,
@@ -30,4 +31,12 @@ if (friendCompanion === true) {
 	console.log("We will travel with " + nameLover + ".")
 };
 
-//
+/*Boolean function:After landing on another planet, an alien starts to attack us for invading their space!
+
+*/
+while (aliensHealth > 0) {
+	console.log ("His health is still at " + aliensHealth + "." + "  He's still alive and coming for us!");
+	aliensHealth --;
+};
+	console.log("We defeated the alien!");
+
