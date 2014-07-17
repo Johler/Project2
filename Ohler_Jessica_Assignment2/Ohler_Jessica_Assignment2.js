7f05e08051c77b6b47b346a79beb6f0a64b2f200
Jessica Ohler <jessicaohler@jessicahlersmbp.gateway.pace.com>
2014-07-17 09:05:15 -0400

    Added Procedure

--- Ohler_Jessica_Assignment2/Ohler_Jessica_Assignment2.js
+++ Ohler_Jessica_Assignment2/Ohler_Jessica_Assignment2.js
@@ -8,9 +8,10 @@
 
 //Set variables
 var partnerName = "Jabba the Pug",
-	expeditionPartner = true
-
-//Prompt
+	expeditionPartner = true,
+	friendCompanion = true
+;
+//Prompt finding out if user would like to travel with a companion.
 myConfirmation = confirm("Would you like to time travel with an expedition partner today?");
 if (expeditionPartner === true) {
 	console.log("Let's decide who to pick!")
@@ -18,4 +19,15 @@ if (expeditionPartner === true) {
 	console.log("Alright, let's get traveling alone!")
 }
 
-//Procedure
+//Procedure which companion to travel with.
+if (friendCompanion === true) {
+	var friendCompanion = true,
+		loverCompanion = false,
+		nameLover = "Joie";
+		
+	console.log("We will travel with " + partnerName + ".")
+	} else {
+	console.log("We will travel with " + nameLover + ".")
+};
+
+//
