e65a1965738f3f62d74e2c6ca18977ce7f14d9d9
Jessica Ohler <jessicaohler@jessicahlersmbp.gateway.pace.com>
2014-07-17 08:45:01 -0400

    added prompt

--- Ohler_Jessica_Assignment2/Ohler_Jessica_Assignment2.js
+++ Ohler_Jessica_Assignment2/Ohler_Jessica_Assignment2.js
@@ -1,1 +1,20 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+// Jessica Ohler
+// SDI 1407
+// Project 2
+// Time Travel Journey
+
+// 
+
+//Set variables
+var partnerName = "Jabba the Pug",
+	expeditionPartner = true
+
+//Prompt
+myConfirmation = confirm("Would you like to time travel with, " + partnerName + " today?");
+if (expeditionPartner === true) {
+	console.log("Let's get Jabba!")
+} else {
+	console.log("Alright, let's get traveling alone!")
+}
+//Procedure
